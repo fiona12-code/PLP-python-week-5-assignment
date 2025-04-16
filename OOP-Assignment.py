5da2bf53ee94416ca8ab1b98e8e46b088ac21504
fiona12-code <fionaomondi77@gmail.com>
2025-04-16 13:42:23 +0300

    python week 5 assignment

---
+++ OOP-Assignment.py
@@ -0,0 +1,16 @@
+class Car:
+     def move(self):
+        return "driving"
+     
+class Boat:
+    def move(self):
+        return "sailing"
+    
+class Airplane:
+    def move(self):
+        return "flying"
+    
+# in action
+for vehicle in [Car(), Boat(), Airplane()]:
+    print(vehicle.move())
+
